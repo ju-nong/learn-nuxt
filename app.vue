5e7d38a86dc3a39b7fdb64e564942cf9ac32495a
이준용 <iiiuooyoo@gmail.com>
2023-03-15 14:53:05 +0900

    init : color-mode, tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <TitleTemp />
+    </div>
 </template>
---
+++ components/TitleTemp.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>어서오고</h1>
+</template>
+
+<script setup></script>
+
+<style lang="scss"></style>
